c92bc75fe94b273c9a270053b87864d53b734145
Amanuel Haile <amanuelhaile369@gmail.com>
2024-12-17 13:43:16 +0300

    answer for 1.10: unicafe step 5 exercise

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,5 +1,16 @@
 import { useState } from 'react'
 
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+
+}
+
+const StatisticLine = (props) => {
+  return <p>{props.text} {props.value}</p>
+
+}
+
 const Statistics = (props) => {
 
   if(!props.good && !props.netural && !props.bad) {
@@ -14,12 +25,12 @@ const Statistics = (props) => {
   return (
     <>
       <h2>statistics</h2>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive}</p>
+      <StatisticLine text="good" value={props.good}/>
+      <StatisticLine text="neutral" value={props.neutral}/>
+      <StatisticLine text="bad" value={props.bad}/>
+      <StatisticLine text="all" value={props.total}/>
+      <StatisticLine text="average" value={props.average}/>
+      <StatisticLine text="positive" value={props.positive}/>
     </>
   );
 }
@@ -47,9 +58,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/>
     </div>
   )
