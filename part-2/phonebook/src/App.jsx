aac879a3ad8c03835dea5e91b35cfafac4090914
Amanuel Haile <amanuelhaile369@gmail.com>
2024-12-18 15:45:42 +0300

    answer for 2.7: The Phonebook Step 2 exercise

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -10,9 +10,16 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
+    const duplicate = persons.filter((person) => person.name === newName);
+    if (duplicate.length > 0) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
     const newPerson = {
       name: newName,
     };
+
     setPersons(persons.concat(newPerson));
     setNewName("");
   };
