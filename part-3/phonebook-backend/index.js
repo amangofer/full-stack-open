51941cf19425cce68683960e89ae9ae982618f54
Amanuel Haile <amanuelhaile369@gmail.com>
2024-12-31 20:01:18 +0300

    answer for 3.9: Phonebook backend step 9 exercise

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("body", function getId(req) {
   return JSON.stringify(req.body);
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(cors());
 
 let persons = [
   {
