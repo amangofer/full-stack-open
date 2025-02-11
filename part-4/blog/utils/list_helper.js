7cbb20efc68a56fd1a3f732ab43a4f0a93226f5f
Amanuel Haile <amanuelhaile369@gmail.com>
2025-02-11 14:44:19 +0300

    solution for 4.5*: Helper Functions and Unit Tests, step 3 exercise

--- part-4/blog/utils/list_helper.js
+++ part-4/blog/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const mostLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const [mostLiked] = blogs.filter((blog) => blog.likes === mostLikes);
+  return mostLiked ? mostLiked : {};
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
