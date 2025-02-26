7c3274ac7d451d9d181efa462448ec8f6ec97ccf
Amanuel Haile <amanuelhaile369@gmail.com>
2025-02-26 21:55:03 +0300

    answer for 4.10: Blog List Tests, step 3 exercise

--- part-4/blog/tests/blogApi.test.js
+++ part-4/blog/tests/blogApi.test.js
@@ -37,6 +37,27 @@ test("the unique identifier property of the blog posts is named id", async () =>
   assert.strictEqual(response.body[0].hasOwnProperty("id"), true);
 });
 
+test("addition of a new blog", async () => {
+  const newBlog = {
+    title: "Tools are not the Answer",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/10/04/CodeIsNotTheAnswer.html",
+    likes: 7,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+
+  const contents = blogsAtEnd.map((b) => b.title);
+  assert(contents.includes("Tools are not the Answer"));
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
