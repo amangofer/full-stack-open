b74bb1aefa196b7b505b3e5a561ff66c35ba1033
Amanuel Haile <amanuelhaile369@gmail.com>
2025-03-10 11:26:34 +0300

    solution for 4.20*: Blog List Expansion, step 8 exercises

--- part-4/blog/app.js
+++ part-4/blog/app.js
@@ -22,6 +22,7 @@ if (!process.env.NODE_ENV === "test") {
 }
 app.use(cors());
 app.use(express.json());
+app.use(middleware.tokenExtractor);
 app.use("/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
