444086f4426c412825ae6c15b59479bd81c6d75b
Amanuel Haile <amanuelhaile369@gmail.com>
2025-03-10 10:53:51 +0300

    solution for 4.19: Blog List Expansion, step 7 exercises

--- part-4/blog/utils/middleware.js
+++ part-4/blog/utils/middleware.js
@@ -21,6 +21,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message });
   } else if (error.code === 11000) {
     return response.status(400).json({ error: "Username already exists" });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(400).json({ error: "invalid token" });
   }
 
   next(error);
