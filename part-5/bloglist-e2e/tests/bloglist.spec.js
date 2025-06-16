713844cf4d1cd149aafd03665c11e37617e40ee9
Amanuel Haile <amanuelhaile369@gmail.com>
2025-06-16 12:36:31 +0300

    solution for 5.18: Blog List End To End Testing, step 2

--- part-5/bloglist-e2e/tests/bloglist.spec.js
+++ part-5/bloglist-e2e/tests/bloglist.spec.js
@@ -1,11 +1,43 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog App", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3001/api/reset");
+    await request.post("http://localhost:3001/api/user", {
+      data: {
+        name: "Matti Luukkainen",
+        username: "mluukkai",
+        password: "salainen",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
   test("Login form is shown", async ({ page }) => {
     await expect(page.getByText("login to application")).toBeVisible();
+    await expect(page.getByRole("button", { name: "login" })).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("mluukkai");
+      await page.getByTestId("password").fill("salainen");
+      await page.getByRole("button", { name: "Login" }).click();
+
+      await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible();
+      await expect(page.getByRole("button", { name: "Logout" })).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("mluukkai");
+      await page.getByTestId("password").fill("wongpassword");
+      await page.getByRole("button", { name: "Login" }).click();
+
+      const errorDiv = page.locator(".error");
+      await expect(errorDiv).toContainText("invalid username or password");
+      await expect(errorDiv).toHaveCSS("border-style", "solid");
+      await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+    });
   });
 });
