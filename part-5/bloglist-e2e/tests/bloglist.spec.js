23c1679bf7e6e91a736cfaa3dc7729cb7d9927a4
Amanuel Haile <amanuelhaile369@gmail.com>
2025-06-16 18:37:54 +0300

    solution for 5.20: Blog List End To End Testing, step 4

--- part-5/bloglist-e2e/tests/bloglist.spec.js
+++ part-5/bloglist-e2e/tests/bloglist.spec.js
@@ -1,9 +1,10 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith, createBlog } = require("./helper");
 
 describe("Blog App", () => {
   beforeEach(async ({ page, request }) => {
-    await request.post("http://localhost:3001/api/reset");
-    await request.post("http://localhost:3001/api/user", {
+    await request.post("http://localhost:3001/api/testing/reset");
+    await request.post("http://localhost:3001/api/users", {
       data: {
         name: "Matti Luukkainen",
         username: "mluukkai",
@@ -43,18 +44,46 @@ describe("Blog App", () => {
 
   describe("When loged in", () => {
     beforeEach(async ({ page }) => {
-      await page.getByTestId("username").fill("mluukkai");
-      await page.getByTestId("password").fill("salainen");
-      await page.getByRole("button", { name: "Login" }).click();
+      await loginWith(page, "mluukkai", "salainen");
     });
-    test("a new blog can be created", async({ page }) => {
-      await page.getByRole("button", {name: "New Blog"}).click()
-      await page.getByTestId("title").fill("First Blog")
-      await page.getByTestId("author").fill("Matti Luukkainen")
-      await page.getByTestId("url").fill("http://example.blog/1")
-      await page.getByRole("button", {name: "create"}).click()
+    test("a new blog can be created", async ({ page }) => {
+      const blog = {
+        title: "First Blog",
+        author: "Matti Luukkainen",
+        url: "http://example.blog/1",
+      };
+      await createBlog(page, blog);
+      await expect(
+        page.getByText(`${blog.title} ${blog.author}`),
+      ).toBeVisible();
+    });
 
-      await expect(page.getByText("First Blog Matti Luukkainen")).toBeVisible()
+    describe("and several notes exists", () => {
+      const blog = {
+        title: "New Blog",
+        author: "Matti Luukkainen",
+        url: "http://example.blog/1",
+      };
+      const blog2 = {
+        title: "New Blog 2",
+        author: "Matti Luukkainen",
+        url: "http://example.blog/1",
+      };
+
+      beforeEach(async ({ page }) => {
+        await createBlog(page, blog);
+        await createBlog(page, blog2);
+      });
+      test("blog can be liked", async ({ page }) => {
+        const blogText = page.getByText("New Blog Matti Luukkainen");
+        const blogDiv = blogText.locator("..");
+
+        await blogDiv.getByRole("button", { name: "view" }).click();
+        await expect(blogDiv.getByText("likes 0")).toBeVisible();
+        await blogDiv.getByRole("button", { name: "like" }).click();
+
+        await expect(blogDiv.getByText("likes 1")).toBeVisible();
+      });
     });
   });
 });
---
+++ part-5/bloglist-e2e/tests/helper.js
@@ -0,0 +1,16 @@
+const loginWith = async (page, username, password) => {
+  await page.getByTestId("username").fill(username);
+  await page.getByTestId("password").fill(password);
+  await page.getByRole("button", { name: "Login" }).click();
+};
+
+const createBlog = async (page, blog) => {
+  await page.getByRole("button", { name: "New Blog" }).click();
+  await page.getByTestId("title").fill(blog.title);
+  await page.getByTestId("author").fill(blog.author);
+  await page.getByTestId("url").fill(blog.url);
+  await page.getByRole("button", { name: "create" }).click();
+  await page.getByRole("button", { name: "cancel" }).click();
+};
+
+export { loginWith, createBlog };
